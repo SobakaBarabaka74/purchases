e81f76cdc0931c87adcdad22340768fe115bd5a5
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2023-12-09 15:55:48 +0700

    Add files via upload
    Добавил фармилки

--- client.js
+++ client.js
@@ -5,7 +5,7 @@
 */
 
 // Создание команд
-Teams.Add('Team', '<b><size=40>T</size><size=30>eam</size></b>', { g: 60, r: 2, b: 12 });
+Teams.Add('Team', '<b><size=40>T</size><size=30>eam</size></b>', { g: 80, r: 2, b: 12 });
 var team = Teams.Get('Team');
 team.Spawns.SpawnPointsGroups.Add(1);
 
@@ -60,3 +60,51 @@ LeaderBoard.PlayerLeaderBoardValues = [
 		ShortDisplayName: '<b>S</b>'
 	}
 ];
+
+// Фармилка
+// В название зоны нужно писать уровень зоны, от 1 до 3
+var furm = AreaPlayerTriggerService.Get('Furm');
+furm.Tags = ['furm'];
+furm.Enable = true;
+furm.OnEnter.Add(function(p, a) {
+	let pt = p.Properties;
+	switch (a.Name) {
+		case '1':
+			p.Timers.Get('furm1').RestartLoop(1);
+			pt.Get('furm').Value = 'furm1';
+			break;
+		case '2':
+			p.Timers.Get('furm2').RestartLoop(5);
+			pt.Get('furm').Value = 'furm2';
+			break;
+		case '3':
+			p.Timers.Get('furm3').RestartLoop(10);
+			pt.Get('furm').Value = 'furm3';
+			break;
+		default:
+			p.Ui.Hint.Value = 'UNKNOWN FURM: имя зоны не соответствует уровню';
+			break;
+	}
+});
+furm.OnExit.Add(function(p) {
+	try {
+		p.Timers.Get(p.Properties.Get('furm').Value).Stop();
+		p.Properties.Get('furm').Value = null;
+	} catch { }
+});
+
+Timers.OnPlayerTimer.Add(function(t) {
+	let p = t.Player;
+	
+	switch (t.Id) {
+		case '1':
+			p.Properties.Get('Scores').Value += 10;
+			break;
+		case '2':
+			p.Properties.Get('Scores').Value += 50;
+			break;
+		case '3':
+			p.Properties.Get('Scores').Value += 100;
+			break;
+	}
+});
